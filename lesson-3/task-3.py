759fb73cbfeab23972817b71ef8dd6cb4fe801ac
Alex <alkostarev@gmail.com>
2020-12-18 12:26:35 +0500

    Optimized code in lesson 3/ task-3

--- lesson-3/task-3.py
+++ lesson-3/task-3.py
@@ -2,8 +2,12 @@
 # которая принимает три позиционных аргумента,
 # и возвращает сумму наибольших двух аргументов.
 def my_func(a, b, c):
-    d = a + b + c - min(a,b,c)
-    return d
+    try:
+        a, b, c = float(a), float(b), float(c)
+        d = a + b + c - min(a,b,c)
+        return d
+    except ValueError:
+        return ('Введите только числа!')
 
-a1,b1, c1 = (int(i) for i in input('Введите числа a, b, c через пробел: ').split())
-print (f'Сумма наибольших двух аргументов равна {my_func(a1,b1,c1)}')
+a1,b1, c1 = (i for i in input('Введите числа a, b, c через пробел: ').split())
+print (f'Сумма наибольших двух аргументов равна: {my_func(a1,b1,c1)}')
